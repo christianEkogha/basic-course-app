14653b6d51026b32221fc74b5d8bc3ae70ee3f43
christianEkogha <vekogha@gmail.com>
2023-11-10 17:22:21 +0100

    Update CourseApplicationTest.java

--- src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
+++ src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
@@ -6,20 +6,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase(replace=Replace.NONE)
 class CourseApplicationTest {
 
-	@Autowired
-	CourseApplication application;
-	
-	@Test
-	void test() {
-		assertThat(application).isNotNull();
-	}
+ @Autowired
+ private CourseApplication application;
+
+ /**
+ * Test the spring context initialization
+ */
+ @Test
+ void test() {
+ assertThat(application).isNotNull();
+ }
 
 }
