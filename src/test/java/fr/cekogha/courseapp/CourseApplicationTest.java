febe0838af6155b6b4ba0d210a1dddaf5aebcfae
christianEkogha <vekogha@gmail.com>
2023-11-10 14:13:26 +0100

    Update CourseApplicationTest.java
    solved test execution issues while maven build

--- src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
+++ src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace=Replace.ANY)
 class CourseApplicationTest {
 
 	@Autowired
