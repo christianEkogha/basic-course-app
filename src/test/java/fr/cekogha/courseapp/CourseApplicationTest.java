8aacf044830d06cbe052cd26f71f964817951d8d
christianEkogha <vekogha@gmail.com>
2023-11-10 14:22:33 +0100

    Update CourseApplicationTest.java

--- src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
+++ src/test/java/fr/cekogha/courseapp/CourseApplicationTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase(replace=Replace.NONE)
